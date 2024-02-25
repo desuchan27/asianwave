5f486512f1d92dfb485b6be114942b111f8bb87d
Desu Chan <orenodesukouichi@gmail.com>
2024-02-25 13:38:15 +0800

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ const Footer = () => {
         <footer className="bg-white border-t border-custom-light-purple">
             <div className="mx-auto py-10">
                 <p className="text-center text-xs text-black">
-                    &copy; 2023 Asianwave, Inc. All rights reserved.
+                    &copy; 2023 Asianwave, Inc. desuchan27 All rights reserved.
                 </p>
             </div>
         </footer>
